10baeb790ca32562a86d33c7dc9a03819158a01f
Roy Willemse <roy.willemse@zimzam.nl>
2015-03-15 12:16:00 +0100

    Revert "GraphicsMagick 1.3.21 including support for WEBP (0.4.3)"
    This reverts commit 62248c151b3b67300cbe43cd9a6ac7c606710701.

--- mysql/test.sh
+++ mysql/test.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 docker rm mysql-test
 docker create -v /var/lib/mysql --name mysql-test dynamind/data-only nop
-docker run -it --volumes-from mysql-test -e MYSQL_ROOT_PASSWORD=changeit --rm mysql /bin/bash
+docker run -it --volumes-from mysql-test -p 3306:3306 -e MYSQL_ROOT_PASSWORD=changeit --rm mysql /bin/bash
